2482926c87db02bd74e3567478bd75ec91f01b2f
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2024-06-13 11:31:48 +0200

    Remove Obsolete Tests

--- util/src/test/java/care/smith/fts/util/HTTPClientConfigTest.java
+++ util/src/test/java/care/smith/fts/util/HTTPClientConfigTest.java
@@ -2,15 +2,10 @@ package care.smith.fts.util;
 
 import static org.assertj.core.api.Assertions.*;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
-import care.smith.fts.util.auth.HTTPClientAuthMethod;
 import care.smith.fts.util.auth.HTTPClientAuthMethod.AuthMethod;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 
 public class HTTPClientConfigTest {
@@ -29,8 +24,7 @@ public class HTTPClientConfigTest {
 
   @Test
   public void nullAuthDoesntThrow() {
-    assertThat(new HTTPClientConfig("http://localhost", null).auth())
-        .isEqualTo(AuthMethod.NONE);
+    assertThat(new HTTPClientConfig("http://localhost", null).auth()).isEqualTo(AuthMethod.NONE);
   }
 
   @Test
@@ -45,21 +39,5 @@ public class HTTPClientConfigTest {
         """;
 
     assertThat(om.readValue(config, HTTPClientConfig.class)).isNotNull();
-  }
-
-  @Test
-  public void hapiClientCreated() {
-    HTTPClientConfig config = new HTTPClientConfig("http://localhost");
-    IGenericClient client = config.createClient(FhirContext.forR4().getRestfulClientFactory());
-
-    assertThat(client).isNotNull();
-  }
-
-  @Test
-  public void apacheClientCreated() {
-    HTTPClientConfig config = new HTTPClientConfig("http://localhost");
-    CloseableHttpClient client = config.createClient(HttpClientBuilder.create());
-
-    assertThat(client).isNotNull();
   }
 }
