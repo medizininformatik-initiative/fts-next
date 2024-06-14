8a7d22e64ec352f3e591096d942fb60842960a41
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2024-06-14 13:52:24 +0200

    Remove IGenericClient from RDA

--- research-domain-agent/src/main/java/care/smith/fts/rda/ResearchDomainAgent.java
+++ research-domain-agent/src/main/java/care/smith/fts/rda/ResearchDomainAgent.java
@@ -1,12 +1,9 @@
 package care.smith.fts.rda;
 
-import static ca.uhn.fhir.rest.client.api.IRestfulClientFactory.*;
 import static com.google.common.base.Strings.isNullOrEmpty;
-import static org.apache.hc.core5.util.Timeout.ofMilliseconds;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
-import ca.uhn.fhir.rest.client.api.IRestfulClientFactory;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
@@ -15,10 +12,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ForkJoinPool;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -38,32 +31,6 @@ public class ResearchDomainAgent {
   @Bean
   public FhirContext fhirContext() {
     return FhirContext.forR4();
-  }
-
-  @Bean
-  public IRestfulClientFactory fhirClientFactory(FhirContext fhir) {
-    IRestfulClientFactory factory = fhir.getRestfulClientFactory();
-    factory.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT);
-    factory.setSocketTimeout(DEFAULT_SOCKET_TIMEOUT);
-    factory.setConnectionRequestTimeout(IRestfulClientFactory.DEFAULT_CONNECTION_REQUEST_TIMEOUT);
-    return factory;
-  }
-
-  @Bean
-  public PoolingHttpClientConnectionManager connectionManager() {
-    return new PoolingHttpClientConnectionManager();
-  }
-
-  @Bean
-  public HttpClientBuilder clientBuilder(PoolingHttpClientConnectionManager manager) {
-    RequestConfig requestConfig =
-        RequestConfig.custom()
-            .setConnectionRequestTimeout(ofMilliseconds(DEFAULT_CONNECTION_REQUEST_TIMEOUT))
-            .setResponseTimeout(ofMilliseconds(DEFAULT_SOCKET_TIMEOUT))
-            .build();
-    return HttpClients.custom()
-        .setConnectionManager(manager)
-        .setDefaultRequestConfig(requestConfig);
   }
 
   @Bean
