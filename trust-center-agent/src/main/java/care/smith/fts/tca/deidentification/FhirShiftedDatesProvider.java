fc3eae995ebdbb9109125dfec63e5a630ef758d0
Jonas Wagner <jwagner@knoppiks.de>
2024-06-21 12:51:31 +0200

    Fix Id Prefix must not be Returned

--- trust-center-agent/src/main/java/care/smith/fts/tca/deidentification/FhirShiftedDatesProvider.java
+++ trust-center-agent/src/main/java/care/smith/fts/tca/deidentification/FhirShiftedDatesProvider.java
@@ -16,6 +16,7 @@ import redis.clients.jedis.params.SetParams;
 
 @Component
 public class FhirShiftedDatesProvider implements ShiftedDatesProvider {
+  private static final String SHIFTED_DATE_PREFIX = "shiftedDate";
   private final JedisPool jedisPool;
 
   public FhirShiftedDatesProvider(JedisPool jedisPool) {
@@ -26,17 +27,21 @@ public class FhirShiftedDatesProvider implements ShiftedDatesProvider {
   public Mono<Map<String, Duration>> generateDateShift(Set<String> ids, Duration dateShiftBy) {
     var shiftByMillis = dateShiftBy.toMillis();
     try (Jedis jedis = jedisPool.getResource()) {
-      return Flux.fromStream(ids.stream().map(id -> "shiftedDate:" + id))
+      return Flux.fromIterable(ids)
           .map(id -> generateIfNotExists(id, jedis, shiftByMillis))
-          .map(id -> new Entry(id, ofMillis(parseLong(jedis.get(id)))))
+          .map(id -> new Entry(id, ofMillis(parseLong(jedis.get(withPrefix(id))))))
           .collectMap(Entry::id, Entry::shift);
     }
   }
 
   private String generateIfNotExists(String id, Jedis jedis, long shiftByMillis) {
     var timeShift = getRandomLong(-shiftByMillis, shiftByMillis);
-    jedis.set(id, String.valueOf(timeShift), new SetParams().nx());
+    jedis.set((withPrefix(id)), String.valueOf(timeShift), new SetParams().nx());
     return id;
+  }
+
+  private static String withPrefix(String id) {
+    return "%s:%s".formatted(SHIFTED_DATE_PREFIX, id);
   }
 
   public long getRandomLong(long lower, long upper) {
