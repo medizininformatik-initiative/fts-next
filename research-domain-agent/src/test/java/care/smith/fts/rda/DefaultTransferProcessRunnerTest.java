4615da0d73b933425630e8d878e62b11dae5846e
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2024-06-21 12:56:31 +0200

    Add Test for DefaultTransferProcessRunner

---
+++ research-domain-agent/src/test/java/care/smith/fts/rda/DefaultTransferProcessRunnerTest.java
@@ -0,0 +1,37 @@
+package care.smith.fts.rda;
+
+import static care.smith.fts.api.rda.BundleSender.*;
+import static reactor.core.publisher.Flux.fromIterable;
+import static reactor.core.publisher.Mono.just;
+import static reactor.test.StepVerifier.create;
+
+import care.smith.fts.api.TransportBundle;
+import care.smith.fts.api.rda.BundleSender;
+import java.util.List;
+import java.util.Set;
+import org.hl7.fhir.r4.model.Bundle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+class DefaultTransferProcessRunnerTest {
+
+  private DefaultTransferProcessRunner runner;
+
+  @BeforeEach
+  void setUp() {
+    runner = new DefaultTransferProcessRunner();
+  }
+
+  @Test
+  void runMockTestSuccessfully() {
+    BundleSender.Result result = new Result(0);
+    TransferProcess process =
+        new TransferProcess(
+            "test", (b) -> fromIterable(List.of(new Bundle())), (b) -> just(result));
+
+    create(runner.run(process, Flux.just(new TransportBundle(new Bundle(), Set.of()))))
+        .expectNext(new TransferProcessRunner.Result())
+        .verifyComplete();
+  }
+}
