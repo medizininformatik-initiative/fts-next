2a96f6f12d30e4b48b5bb9f981a2d4a1f38890a2
Jonas Wagner <jwagner@knoppiks.de>
2024-06-25 10:55:11 +0200

    Customize toString Method for Logging

--- clinical-domain-agent/src/main/java/care/smith/fts/cda/TransferProcessConfig.java
+++ clinical-domain-agent/src/main/java/care/smith/fts/cda/TransferProcessConfig.java
@@ -7,4 +7,15 @@ public record TransferProcessConfig(
     @NotNull Map<String, ?> cohortSelector,
     @NotNull Map<String, ?> dataSelector,
     @NotNull Map<String, ?> deidentificationProvider,
-    @NotNull Map<String, ?> bundleSender) {}
+    @NotNull Map<String, ?> bundleSender) {
+
+  @Override
+  public String toString() {
+    return "TransferProcessConfig{cohortSelector=%s, dataSelector=%s, deidentificationProvider=%s, bundleSender=%s}"
+        .formatted(
+            cohortSelector.keySet(),
+            dataSelector.keySet(),
+            deidentificationProvider.keySet(),
+            bundleSender.keySet());
+  }
+}
--- research-domain-agent/src/main/java/care/smith/fts/rda/TransferProcessConfig.java
+++ research-domain-agent/src/main/java/care/smith/fts/rda/TransferProcessConfig.java
@@ -4,4 +4,11 @@ import jakarta.validation.constraints.NotNull;
 import java.util.Map;
 
 public record TransferProcessConfig(
-    @NotNull Map<String, ?> deidentificationProvider, @NotNull Map<String, ?> bundleSender) {}
+    @NotNull Map<String, ?> deidentificationProvider, @NotNull Map<String, ?> bundleSender) {
+
+  @Override
+  public String toString() {
+    return "TransferProcessConfig{deidentificationProvider=%s, bundleSender=%s}"
+        .formatted(deidentificationProvider.keySet(), bundleSender.keySet());
+  }
+}
