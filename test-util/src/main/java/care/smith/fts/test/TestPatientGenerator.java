f4f6eed77832998d2d985e70948addad8d223eeb
Jonas Wagner <jwagner@knoppiks.de>
2024-06-28 09:48:21 +0200

    Remove Unused Generator Method

--- test-util/src/main/java/care/smith/fts/test/TestPatientGenerator.java
+++ test-util/src/main/java/care/smith/fts/test/TestPatientGenerator.java
@@ -1,10 +1,7 @@
 package care.smith.fts.test;
 
 import care.smith.fts.test.FhirGenerator.Fixed;
-import care.smith.fts.test.FhirGenerator.Increasing;
-import care.smith.fts.test.FhirGenerator.OneOfList;
 import java.io.IOException;
-import java.util.List;
 import org.hl7.fhir.r4.model.Bundle;
 
 public class TestPatientGenerator {
@@ -14,15 +11,6 @@ public class TestPatientGenerator {
     fhirGenerator.replaceTemplateFieldWith("$PATIENT_ID", new Fixed(id));
     fhirGenerator.replaceTemplateFieldWith("$IDENTIFIER_SYSTEM", new Fixed(identifierSystem));
     fhirGenerator.replaceTemplateFieldWith("$YEAR", new Fixed(year));
-    return fhirGenerator.generateBundle(1, 100);
-  }
-
-  public static Bundle generatePatients(
-      String idPrefix, List<String> years, String identifierSystem) throws IOException {
-    FhirGenerator fhirGenerator = new FhirGenerator("PatientTemplate.json");
-    fhirGenerator.replaceTemplateFieldWith("$PATIENT_ID", new Increasing(idPrefix));
-    fhirGenerator.replaceTemplateFieldWith("$IDENTIFIER_SYSTEM", new Fixed(identifierSystem));
-    fhirGenerator.replaceTemplateFieldWith("$YEAR", new OneOfList(years));
     return fhirGenerator.generateBundle(1, 100);
   }
 }
