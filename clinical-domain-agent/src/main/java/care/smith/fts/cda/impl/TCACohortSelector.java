a3dc778cb35122fcc7b0ea013078a91f876d6737
Jonas Wagner <jwagner@knoppiks.de>
2024-07-10 07:19:46 +0200

    Refactor TCACohortSelector

--- clinical-domain-agent/src/main/java/care/smith/fts/cda/impl/TCACohortSelector.java
+++ clinical-domain-agent/src/main/java/care/smith/fts/cda/impl/TCACohortSelector.java
@@ -1,18 +1,22 @@
 package care.smith.fts.cda.impl;
 
-import static care.smith.fts.util.ConsentedPatientExtractor.extractConsentedPatients;
 import static care.smith.fts.util.MediaTypes.APPLICATION_FHIR_JSON;
+import static java.util.Map.entry;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 import care.smith.fts.api.ConsentedPatient;
 import care.smith.fts.api.cda.CohortSelector;
+import care.smith.fts.util.ConsentedPatientExtractor;
 import care.smith.fts.util.error.TransferProcessException;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientException;
 import reactor.core.publisher.Flux;
@@ -33,38 +37,43 @@ class TCACohortSelector implements CohortSelector {
     return client
         .post()
         .uri("/api/v2/cd/consented-patients")
-        .bodyValue(
-            Map.of(
-                "policies",
-                config.policies(),
-                "policySystem",
-                config.policySystem(),
-                "domain",
-                config.domain()))
+        .bodyValue(constructBody(config.policies(), config.policySystem(), config.domain()))
         .headers(h -> h.setContentType(APPLICATION_JSON))
         .headers(h -> h.setAccept(List.of(APPLICATION_FHIR_JSON)))
         .retrieve()
-        .onStatus(
-            r -> r.equals(HttpStatus.BAD_REQUEST),
-            r ->
-                r.bodyToMono(ProblemDetail.class)
-                    .flatMap(b -> Mono.error(new TransferProcessException(b.getDetail()))))
+        .onStatus(r -> r.equals(HttpStatus.BAD_REQUEST), TCACohortSelector::handleBadRequest)
         .bodyToMono(Bundle.class)
         // TODO Paging using .expand()? see Flare
         .doOnNext(b -> log.debug("Found {} consented patient bundles", b.getEntry().size()))
         .doOnError(e -> log.error(e.getMessage()))
+        .onErrorResume(WebClientException.class, TCACohortSelector::handleError)
+        .flatMapMany(this::extractConsentedPatients);
+  }
+
+  private Map<String, Object> constructBody(
+      Set<String> policies, @NotNull String v2, String domain) {
+    return Map.ofEntries(
+        entry("policies", policies), entry("policySystem", v2), entry("domain", domain));
+  }
+
+  private Flux<ConsentedPatient> extractConsentedPatients(Bundle outerBundle) {
+    return Flux.fromStream(
+        ConsentedPatientExtractor.extractConsentedPatients(
+            config.patientIdentifierSystem(),
+            config.policySystem(),
+            outerBundle,
+            config.policies()));
+  }
+
+  private static Mono<Bundle> handleError(WebClientException e) {
+    return Mono.error(
+        new TransferProcessException("Error communicating with trust center agent", e));
+  }
+
+  private static Mono<Throwable> handleBadRequest(ClientResponse r) {
+    return r.bodyToMono(ProblemDetail.class)
+        .flatMap(b -> Mono.<Throwable>error(new TransferProcessException(b.getDetail())))
         .onErrorResume(
-            WebClientException.class,
-            e ->
-                Mono.error(
-                    new TransferProcessException("Error communicating with trust center agent", e)))
-        .flatMapMany(
-            outerBundle ->
-                Flux.fromStream(
-                    extractConsentedPatients(
-                        config.patientIdentifierSystem(),
-                        config.policySystem(),
-                        outerBundle,
-                        config.policies())));
+            e -> Mono.error(new TransferProcessException("Unable to parse ProblemDetail", e)));
   }
 }
