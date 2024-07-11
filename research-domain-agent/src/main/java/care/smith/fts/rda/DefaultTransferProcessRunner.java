64209558db9e7a363d509002db19396b79952bfc
Jonas Wagner <jwagner@knoppiks.de>
2024-07-11 12:07:13 +0200

    Add Missing Override Annotation

--- research-domain-agent/src/main/java/care/smith/fts/rda/DefaultTransferProcessRunner.java
+++ research-domain-agent/src/main/java/care/smith/fts/rda/DefaultTransferProcessRunner.java
@@ -12,6 +12,7 @@ import reactor.core.publisher.Mono;
 @Component
 public class DefaultTransferProcessRunner implements TransferProcessRunner {
 
+  @Override
   public Mono<Result> run(TransferProcessDefinition process, Mono<TransportBundle> data) {
     return runProcess(data, process.bundleSender(), process.deidentificator());
   }
