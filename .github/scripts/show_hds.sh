03908e1745bfc3500d07ab63c2cccf04e671bf51
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2024-08-05 11:17:53 +0200

    Add Script That Shows HDS' Content and Cmd to Clear RD-HDS

---
+++ .github/scripts/show_hds.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+set -euo pipefail
+
+
+if ! cd_hds_base_url="http://$(docker compose port cd-hds 8080)/fhir"; then
+  >&2 echo "Unable to find clinical domain health data store URL"
+  exit 2
+fi
+
+
+if ! rd_hds_base_url="http://$(docker compose port rd-hds 8080)/fhir"; then
+  >&2 echo "Unable to find research domain health data store URL"
+  exit 2
+fi
+
+blazectl count-resources --server "${cd_hds_base_url}"
+blazectl count-resources --server "${rd_hds_base_url}"
