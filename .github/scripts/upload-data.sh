6326da6d73e34a1cf888468b1ee3bca7b85de3c7
Jonas Wagner <jwagner@knoppiks.de>
2024-10-01 08:34:38 +0200

    Upload Test Data in Parallel

--- .github/scripts/upload-data.sh
+++ .github/scripts/upload-data.sh
@@ -10,23 +10,19 @@ if ! cd_hds_base_url="http://$(docker compose port cd-hds 8080)/fhir"; then
   exit 2
 fi
 
-function transfer_test_data() {
+function transfer_file() {
   curl -sf "${share}/download?path=kds&files=${1}.json.gz" | gunzip \
   | curl -sf --data-binary @- -H "Content-Type: application/fhir+json" "${cd_hds_base_url}" \
   >/dev/null
 }
+export -f transfer_file
+export share
+export cd_hds_base_url
 
-transfer_test_data "hospitalInformation"
-transfer_test_data "practitionerInformation"
+echo "Uploading patients to cd-hds"
+{ echo "hospitalInformation";
+  echo "practitionerInformation";
+  curl -sf "${share}/download?files=authored.json" | jq -rc "to_entries | .[0:${1:-10}] | .[].key"
+} | xargs -P8 -I{} bash -c  'transfer_file "{}"'
 
-echo "Uploading consent to gics and patients to cd-hds"
-i=1
-while read -r patient; do
-  patient_id="$(echo "${patient}" | jq -r .key)"
-
-  transfer_test_data "${patient_id}"
-
-  printf "%s: %8d / %d\n" "${patient_id}" $((i++)) "${1:-10}"
-done < <(curl -sf "${share}/download?files=authored.json" \
-           | jq -c 'to_entries | .[]' | head -n "${1:-10}")
 echo "Upload finished"
