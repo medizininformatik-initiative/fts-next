541f725e286a4d775f1221ad14d5533f5d3aa5b8
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2024-11-27 14:58:58 +0100

    Update to Use MockitoBean

--- trust-center-agent/src/test/java/care/smith/fts/tca/deidentification/configuration/GpasFhirDeIdentificationConfigurationTest.java
+++ trust-center-agent/src/test/java/care/smith/fts/tca/deidentification/configuration/GpasFhirDeIdentificationConfigurationTest.java
@@ -8,7 +8,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -16,7 +16,7 @@ class GpasFhirDeIdentificationConfigurationTest {
 
   @Autowired private GpasFhirDeIdentificationConfiguration gpasFhirDeIdentificationConfiguration;
 
-  @MockBean
+  @MockitoBean
   RedissonClient redisClient; // We need to mock the redisClient otherwise the tests won't start
 
   @Test
