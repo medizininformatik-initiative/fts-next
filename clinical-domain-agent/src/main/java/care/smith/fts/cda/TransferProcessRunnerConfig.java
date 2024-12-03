04794b1592a1f4098767a1319d84c3a7a9f72e5c
Jonas Wagner <jwagner@knoppiks.de>
2024-12-03 09:44:08 +0100

    Fix Release Artifact Assembly

--- clinical-domain-agent/src/main/java/care/smith/fts/cda/TransferProcessRunnerConfig.java
+++ clinical-domain-agent/src/main/java/care/smith/fts/cda/TransferProcessRunnerConfig.java
@@ -4,13 +4,17 @@ import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties("runner")
 @Setter
 public class TransferProcessRunnerConfig {
+
+  @NestedConfigurationProperty
   @NotNull int maxSendConcurrency = 128;
+
   @NotNull int maxConcurrentProcesses = 4;
   @NotNull Duration processTtl = Duration.ofDays(1);
 }
