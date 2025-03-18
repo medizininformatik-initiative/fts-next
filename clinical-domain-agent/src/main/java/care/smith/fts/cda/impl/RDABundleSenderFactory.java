54f13275edee4dcc43a6c82b3d408de1dfd3c751
Jonas Wagner <jwagner@knoppiks.de>
2025-03-18 15:58:33 +0100

    Add Logging to Recognize RDA Client

--- clinical-domain-agent/src/main/java/care/smith/fts/cda/impl/RDABundleSenderFactory.java
+++ clinical-domain-agent/src/main/java/care/smith/fts/cda/impl/RDABundleSenderFactory.java
@@ -3,8 +3,10 @@ package care.smith.fts.cda.impl;
 import care.smith.fts.api.cda.BundleSender;
 import care.smith.fts.util.WebClientFactory;
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component("researchDomainAgentBundleSender")
 public class RDABundleSenderFactory implements BundleSender.Factory<RDABundleSenderConfig> {
 
@@ -23,7 +25,8 @@ public class RDABundleSenderFactory implements BundleSender.Factory<RDABundleSen
 
   @Override
   public BundleSender create(BundleSender.Config commonConfig, RDABundleSenderConfig implConfig) {
-    return new RDABundleSender(
-        implConfig, clientFactory.create(implConfig.server()), meterRegistry);
+    var client = clientFactory.create(implConfig.server());
+    log.info("Created Client {}", client);
+    return new RDABundleSender(implConfig, client, meterRegistry);
   }
 }
