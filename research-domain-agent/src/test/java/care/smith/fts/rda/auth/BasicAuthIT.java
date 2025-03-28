a03b6357a08a41414db839e2fbeddb5545bf4512
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2025-03-28 10:02:14 +0100

    Implement Custom Auth Tests

---
+++ research-domain-agent/src/test/java/care/smith/fts/rda/auth/BasicAuthIT.java
@@ -0,0 +1,51 @@
+package care.smith.fts.rda.auth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static reactor.test.StepVerifier.create;
+
+import care.smith.fts.rda.ResearchDomainAgent;
+import care.smith.fts.test.TestWebClientFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException.Unauthorized;
+
+@Slf4j
+@SpringBootTest(classes = ResearchDomainAgent.class, webEnvironment = RANDOM_PORT)
+@Import(TestWebClientFactory.class)
+@ActiveProfiles("auth:basic")
+public class BasicAuthIT {
+
+  private WebClient client;
+
+  @BeforeEach
+  final void setUpTransferProcessControllerIT(
+      @LocalServerPort int port, @Autowired TestWebClientFactory factory) {
+    client = factory.webClient("https://localhost:" + port);
+  }
+
+  @Test
+  void failWithWrongAuth(@LocalServerPort int port, @Autowired TestWebClientFactory factory) {
+    var client = factory.unauthorizedWebClient("https://localhost:" + port);
+    create(client.get().uri("/api/v2/projects").retrieve().bodyToMono(String.class))
+        .expectError(Unauthorized.class)
+        .verify();
+  }
+
+  @Test
+  void projects() {
+    create(client.get().uri("/api/v2/projects").retrieve().bodyToMono(String.class))
+        .assertNext(
+            transferProcessDefinitions -> {
+              assertThat(transferProcessDefinitions).isEqualTo("[\"example\"]");
+            })
+        .verifyComplete();
+  }
+}
