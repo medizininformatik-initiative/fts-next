5c9aff38c7913eceafd4dd72351d74a76e9b4cc6
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2025-03-28 12:46:11 +0100

    Make Test Abstract and Impl for CDA and TCA too

--- test-util/src/main/java/care/smith/fts/test/AbstractAuthIT.java
+++ test-util/src/main/java/care/smith/fts/test/AbstractAuthIT.java
@@ -1,11 +1,8 @@
-package care.smith.fts.rda.auth;
+package care.smith.fts.test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static reactor.test.StepVerifier.create;
 
-import care.smith.fts.rda.ResearchDomainAgent;
-import care.smith.fts.test.TestWebClientFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,39 +10,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
 import org.springframework.web.reactive.function.client.WebClientResponseException.Unauthorized;
 
 @Slf4j
-@SpringBootTest(classes = ResearchDomainAgent.class, webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(TestWebClientFactory.class)
-@ActiveProfiles("auth:oauth2")
-public class OAuth2AuthIT {
+public abstract class AbstractAuthIT {
 
   private WebClient client;
+
+  private final String webclientName;
+
+  public AbstractAuthIT() {
+    this.webclientName = "default";
+  }
+
+  public AbstractAuthIT(String webclientName) {
+    this.webclientName = webclientName;
+  }
+
+  protected abstract RequestHeadersSpec<?> protectedEndpoint(WebClient client);
 
   @BeforeEach
   final void setUpTransferProcessControllerIT(
       @LocalServerPort int port, @Autowired TestWebClientFactory factory) {
-    client = factory.webClient("https://localhost:" + port);
+    client = factory.webClient("https://localhost:" + port, webclientName);
   }
 
   @Test
   void failWithWrongAuth(@LocalServerPort int port, @Autowired TestWebClientFactory factory) {
     var client = factory.unauthorizedWebClient("https://localhost:" + port);
-    create(client.get().uri("/api/v2/projects").retrieve().bodyToMono(String.class))
+    create(protectedEndpoint(client).retrieve().bodyToMono(String.class))
         .expectError(Unauthorized.class)
         .verify();
   }
 
   @Test
-  void projects() {
-    create(client.get().uri("/api/v2/projects").retrieve().bodyToMono(String.class))
-        .assertNext(
-            transferProcessDefinitions -> {
-              assertThat(transferProcessDefinitions).isEqualTo("[\"example\"]");
-            })
+  void publicEndpointRequiresNoAuth(
+      @LocalServerPort int port, @Autowired TestWebClientFactory factory) {
+    var client = factory.unauthorizedWebClient("https://localhost:" + port);
+    create(client.get().uri("/actuator/health").retrieve().bodyToMono(String.class))
+        .expectNextCount(1)
+        .verifyComplete();
+  }
+
+  @Test
+  void authorizedClientCanAccessProtectedEndpoint() {
+    create(protectedEndpoint(client).retrieve().bodyToMono(String.class))
+        .expectNextCount(1)
         .verifyComplete();
   }
 }
