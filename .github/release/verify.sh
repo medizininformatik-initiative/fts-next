1ec9af6f56a1e96c685a6218f81fd806eda40052
Jonas Wagner <jwagner@knoppiks.de>
2025-05-15 11:27:11 +0200

    Sign Docker Images

---
+++ .github/release/verify.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+project="github.com/medizininformatik-initiative/fts-next"
+image="$(grep ghcr.io/medizininformatik-initiative/fts/ <compose.yaml | cut -d':' -f2- | xargs)"
+tag="${1:-<COMMIT_TAG>}"
+
+echo -e "# Verifying image provenance, assert image was built \n- from ${project} \n- for tag ${tag}"
+
+if cosign --help &>/dev/null; then
+  echo -n "* Using cosign "
+  if cosign verify --output-file /dev/null "${image}" \
+    --certificate-identity-regexp "https://${project}/.*" \
+    --certificate-oidc-issuer "https://token.actions.githubusercontent.com" \
+    --certificate-github-workflow-ref="refs/tags/${tag}" &>/dev/null; then
+    echo "✔ "
+  else
+    echo "✘ Verification failed"
+    exit 1
+  fi
+else
+  1>&2 echo "Verification failed. cosign is not installed."
+  1>&2 echo "  Please see https://docs.sigstore.dev/cosign/system_config/installation/"
+  exit 1
+fi
