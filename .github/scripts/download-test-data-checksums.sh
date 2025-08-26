19e0d04a20c0447531b96afea507049bf8eb01f8
Daniel Hahne <daniel.hahne@uni-leipzig.de>
2025-08-26 11:59:25 +0200

    Fix Cohort Selection via FHIR

--- .github/scripts/download-test-data-checksums.sh
+++ .github/scripts/download-test-data-checksums.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 base_url="https://speicherwolke.uni-leipzig.de/public.php/webdav"
 share="${1}"
 
-mkdir -p ./test-data
-curl -sLf -u "${share}:" "${base_url}/kds/checksums.sha256" >./test-data/checksums.sha256
+# Create directory based on share ID to avoid file collisions
+data_dir="./test-data/${share}"
+mkdir -p "${data_dir}"
+curl -sLf -u "${share}:" "${base_url}/kds/checksums.sha256" >"${data_dir}/checksums.sha256"
